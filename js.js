c0bf759fc8f94ba2dc69c0761ee6286bffaf4b1b
Tian34 <70531781+Tian34@users.noreply.github.com>
2021-02-11 22:09:57 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+
+var n1 = document.getElementById("t1");
+var n2 = document.getElementById("t2");
+var n3 = document.getElementById("t");
+var n4 = document.getElementById("b1");
+var n5 = document.getElementById("b2");
+
+		function cambio() {
+			n1.className = "no";
+			n2.className = "";
+			n3.className = "texto";
+			n4.className = "no";
+			n5.className = "boton";
+		}
+
+var n1 = document.getElementById("t1");
+var n2 = document.getElementById("t2");
+var n3 = document.getElementById("t");
+var n4 = document.getElementById("b1");
+var n5 = document.getElementById("b2");
+
+		function cambio1() {
+			n1.className = "";
+			n2.className = "no";
+			n3.className = "texto1";
+			n4.className = "boton";
+			n5.className = "no";
+		}
